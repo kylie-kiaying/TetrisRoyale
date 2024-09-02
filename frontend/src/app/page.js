3c278d217d36c97fa3963072b1817905f5886fbf
Keegan Ravindran <keegan.r.personal@gmail.com>
2024-09-02 22:07:29 +0800

    Landing page styling

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,18 @@
 import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu";
 
 export default function Home() {
   return (
 
     <div className="relative h-screen w-full overflow-hidden">
+        {/* Background Video */}
         <video
         autoPlay
         loop
@@ -12,15 +21,26 @@ export default function Home() {
             <source src="/bgvid.mp4" type="video/mp4" />
             Your browser does not support the video tag
         </video>
-    </div>
 
     
+    {/* Content */}
     <div className="relative z-10 flex flex-col justify-center items-center h-full">
         <h1 className="text-white text-6xl font-bebas mb-12"> Welcome to TetriTracker</h1>
         <div className="flex space-x-4">
+        <DropdownMenu>
+            <DropdownMenuTrigger>
             <Button className="bg-white text-[#1e0b38] hover:bg-gray-200 font-bold py-2 px-4 rounded" >
                 Sign in
             </Button>
+            </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>Sign in as</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Tournament Organizer</DropdownMenuItem>
+                    <DropdownMenuItem>Player</DropdownMenuItem>
+                </DropdownMenuContent>
+</DropdownMenu>
+            
             <Button className="bg-white text-[#1e0b38] hover:bg-gray-200 font-bold py-2 px-4 rounded" >
                 Register new account
             </Button>
@@ -28,7 +48,7 @@ export default function Home() {
     </div>
 
      {/* Overlay for darker effect */}
-     <div className="absolute inset-0 bg-black opacity-50"></div>
+     <div className="absolute inset-0 bg-[#140221] opacity-80"></div>
      </div>
 
   );
