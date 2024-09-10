68cec441074f8159ebb39c53b976553067d9fa4c
Kkskeng <kkseah.2023@scis.smu.edu.sg>
2024-09-11 01:04:46 +0800

    'Create' button

--- frontend/src/app/adminHome/page.jsx
+++ frontend/src/app/adminHome/page.jsx
@@ -27,7 +27,12 @@ export default function adminPage() {
                 <div className="flex justify-center min-w-full items-start h-screen bg-[#0b051d] pt-14">
                     <Card className="bg-opacity-40 w-[1000px] max-w-screen-xl backdrop-blur-md  rounded-lg shadow-lg items-center">
                         <CardHeader>
-                            <CardTitle>Current Tournaments</CardTitle>
+                            <span>
+                                <CardTitle>Current Tournaments</CardTitle>
+                                <span className="absolute top-3 right-5">
+                                <Button variant="outline" className="bg-white text-[#1e0b38] hover:bg-gray-300/70">Create</Button>
+                                </span>
+                            </span>
                         </CardHeader>
                         <CardContent>
                             <Card className="justify-center bg-black/25 max-w-[1000px] backdrop-blur-md  rounded-lg shadow-lg items-center">
@@ -36,19 +41,18 @@ export default function adminPage() {
                                         <CardTitle>
                                             Tournament 1
                                         </CardTitle>
-                                        <span className="absolute top-2 right-5">
-                                        <Button variant="outline" className="bg-white text-[#1e0b38] hover:bg-gray-300/70">Details</Button>
+                                        <span className="absolute top-5 right-14">
+                                            <Button variant="outline" className="bg-white text-[#1e0b38] hover:bg-gray-300/70">Details</Button>
                                         </span>
                                     </span>
                                     <span className="text-xs inline">Status: <span className="text-xs text-yellow-200">Ongoing</span></span>
                                 </CardHeader>
                                 <CardContent>
                                     <span>Estimated Start-End Time: <span>25-9-2024 1300-1400</span></span>
-                                    <span className="absolute bottom-6 right-5">  
+                                    <span className="absolute bottom-4 right-5">  
                                         <Button variant="outline" className="bg-white text-[#1e0b38] hover:bg-gray-300/70">Edit</Button>
                                         <Button variant="outline" className="ml-2 bg-white text-[#1e0b38] hover:bg-gray-300/70">Delete</Button>
                                     </span>
-                                    
                                 </CardContent>
                             </Card>
                         </CardContent>
