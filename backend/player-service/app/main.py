ad7c23872399c457f5a7a18766b6d0402b3a9613
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-09-29 15:30:03 +0800

    Added cors middleware for backend

--- backend/player-service/app/main.py
+++ backend/player-service/app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controller import player_controller
 from app.utils.db import engine
 from app.model.player_model import Base
@@ -11,7 +12,13 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(player_controller.router)
 
 @app.get("/")
