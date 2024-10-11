eaa90e889b7b5b08e3061e8e6b93eb56b45d546e
skiesareredred <dk.yeo.2023@smu.edu.sg>
2024-10-11 20:07:33 +0800

    Added retrieve username function

--- backend/auth-service/app/controller/auth_controller.py
+++ backend/auth-service/app/controller/auth_controller.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException, Depends, Request
+from fastapi.responses import JSONResponse
 from app.schema.auth_schema import UserReg, UserLogin, LoginResponse
 from app.service.auth_service import AuthService
 from app.db.session import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.utils.token_utils import verify_user_role
+from app.utils.token_utils import verify_user_role, retrieve_username
 
 router = APIRouter()
 
@@ -23,8 +24,16 @@ async def register_user(auth_data: UserReg, db: AsyncSession = Depends(get_db), 
 async def verify_email(token: str, db: AsyncSession = Depends(get_db), auth_service: AuthService = Depends()):
     return await auth_service.verify_email(token, db)
 
-@router.get("/protected-route")
-async def protected_route(request: Request):
-    # Verify if the user has the required role to access this route
+# @router.get("/protected-route")
+# async def protected_route(request: Request):
+#     # Verify if the user has the required role to access this route
     
-    return verify_user_role(request, required_role="admin")
+#     return verify_user_role(request, required_role="admin")
+
+# @router.get("/username")
+# async def get_username(request: Request):
+#     try:
+#         username = retrieve_username(request)
+#         return JSONResponse(content={"username": username})
+#     except HTTPException as e:
+#         return JSONResponse(status_code=e.status_code, content={"detail": e.detail})
