96caf1600809a427393405cac0e073a66b565427
skiesareredred <dk.yeo.2023@smu.edu.sg>
2024-10-25 20:51:28 +0800

    Matchmaking function now matches players by nearest rating

--- backend/matchmaking-service/app/service/matchmaking_service.py
+++ backend/matchmaking-service/app/service/matchmaking_service.py
@@ -20,17 +20,23 @@ class MatchmakingService:
         if len(registered_player_ids) < 2:
             raise ValueError("Not enough players registered for pairing")
 
-        random.shuffle(registered_player_ids)
+        players = []
+        async with httpx.AsyncClient() as client:
+            for i in range(0, len(registered_player_ids)):
+                response = await client.get(f"http://rating-service:8000/ratings/{registered_player_ids[i]}")
+                response.raise_for_status()
+                player_rating = response.json()
+                players.append({"id":registered_player_ids[i], "rating":player_rating["rating"]})
 
-        player_pairs = []
-        for i in range(0, len(registered_player_ids), 2):
-            if i + 1 < len(registered_player_ids):
-                player1_id = registered_player_ids[i]
-                player2_id = registered_player_ids[i + 1]
-                match = await self.matchmaking_repository.create_match(tournament_id, player1_id, player2_id)
-                player_pairs.append(match)
+        players = sorted(players, key=lambda x: x["rating"])
 
-        return player_pairs
+        matches = []
+        for i in range(0, len(players) - 1, 2):
+            player1 = players[i]
+            player2 = players[i + 1]
+            matches.append([tournament_id, player1['id'], player1['rating'], player2['id'], player2['rating']])
+
+        return matches
 
     async def get_matches_by_tournament(self, tournament_id: int):
         return await self.matchmaking_repository.get_matches_by_tournament(tournament_id)
