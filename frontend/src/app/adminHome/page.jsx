0abeb4aad31133742181917d5f81bcb867dff394
Kkskeng <kkseah.2023@scis.smu.edu.sg>
2024-11-03 19:30:49 +0800

    edit modal + datetime conversion

--- frontend/src/app/adminHome/page.jsx
+++ frontend/src/app/adminHome/page.jsx
@@ -14,31 +14,47 @@ const tournaments = [
   {
     tournament_id: '1',
     tournament_name: 'Tetris Championship',
-    tournament_start: 'June 1',
-    tournament_end: 'June 10',
+    tournament_start: '2024-11-12T19:30',
+    tournament_end: '2024-11-25T21:00',
+    remarks: 'Champions of Tetris!',
     status: 'Ongoing',
   },
   {
     tournament_id: '2',
     tournament_name: 'Spring Showdown',
-    tournament_start: 'October 5',
-    tournament_end: 'October 15',
+    tournament_start: '2025-01-1T13:30',
+    tournament_end: '2025-01-5T15:30',
+    remarks: 'Showdown of Spring!',
     status: 'Upcoming',
   },
   {
     tournament_id: '3',
     tournament_name: 'Championship Series 1',
-    tournament_start: 'May 25',
-    tournament_end: 'May 31',
+    tournament_start: '2024-06-1T09:30',
+    tournament_end: '2024-06-1T11:30',
+    remarks: 'Series of Champions!',
     status: 'Finished',
   },
 ];
 
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+function convertDateTime(datetime) {
+    let t = datetime.indexOf('T');
+    let dateStr = datetime.substring(0, t);
+    let date = dateStr.split("-");
+    let time = datetime.substring(t+1);
+    return date[2] + " " + months[Number(date[1]) - 1] + " " + date[0] + " " + time;
+}
+
+
+
 export default function AdminPage() {
-  let [open, setOpen] = useState(undefined);
+  let [openDelete, setOpenDelete] = useState(undefined);
   let [create, setCreate] = useState(false);
-  const handleClose = () => setOpen(undefined);
-  const handleShow = (id) => setOpen(id);
+  let [openEdit, setOpenEdit] = useState(undefined);
+
+  const handleCloseDelete = () => setOpenDelete(undefined);
+  const handleCloseEdit = () => setOpenEdit(undefined); 
 
   return (
     <div
@@ -63,65 +79,65 @@ export default function AdminPage() {
               Create
             </Button>
             <Dialog open={create} onClose={() => setCreate(false)} className="relative z-10 rounded-lg">
-                        <DialogBackdrop
-                          transition
-                          className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
-                        />
+              <DialogBackdrop
+                transition
+                className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+              />
 
-                        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                            <DialogPanel
-                              transition
-                              className="relative transform overflow-hidden rounded-lg bg-[#1c1132] text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
-                            >
-                              <div className="bg-[#1c1132] px-4 pb-4 pt-5 sm:p-6 sm:pb-4 text-white">
-                                <div className="sm:items-start">
-                                  <div className="mt-3 text-center sm:mt-0 sm:text-left">
-                                    <DialogTitle as="h3" className="text-base font-semibold leading-6 text-white">
-                                      Create Tournament
-                                    </DialogTitle>
-                                    <div className="mt-2">
-                                      <div className="flex flex-col space-y-1.5 mb-3">
-                                        <Label htmlFor="name">Tournament Name</Label>
-                                        <Input id="name" placeholder="" />
-                                      </div>
-                                      <div className="flex flex-col space-y-1.5 mb-3">
-                                        <Label htmlFor="remark">Remarks</Label>
-                                        <Input id="remark" placeholder="" />
-                                      </div>
-                                      <div className="flex flex-col space-y-1.5 mb-3 w-fit">
-                                        <Label htmlFor="startTime">Tournament Start DateTime</Label>
-                                        <Input id="startTime" type="datetime-local" />
-                                      </div>
-                                      <div className="flex flex-col space-y-1.5 mb-3 w-fit">
-                                        <Label htmlFor="endTime">Tournament End DateTime</Label>
-                                        <Input id="endTime" type="datetime-local" />
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                              <div className="bg-black/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                <button
-                                  type="button"
-                                  onClick={() => setCreate(false)}
-                                  className="inline-flex w-full justify-center rounded-md bg-purple-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-600 sm:ml-3 sm:w-auto"
-                                >
-                                  Create
-                                </button>
-                                <button
-                                  type="button"
-                                  data-autofocus
-                                  onClick={() => setCreate(false)}
-                                  className="mt-3 border border-purple-500 text-purple-500 transition-all duration-200 hover:bg-gray-300/70 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm sm:mt-0 sm:w-auto"
-                                >
-                                  Cancel
-                                </button>
-                              </div>
-                            </DialogPanel>
+              <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                  <DialogPanel
+                    transition
+                    className="relative transform overflow-hidden rounded-lg bg-[#1c1132] text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+                  >
+                    <div className="bg-[#1c1132] px-4 pb-4 pt-5 sm:p-6 sm:pb-4 text-white">
+                      <div className="sm:items-start">
+                        <div className="mt-3 text-center sm:mt-0 sm:text-left">
+                          <DialogTitle as="h3" className="text-base font-semibold leading-6 text-white">
+                            Create Tournament
+                          </DialogTitle>
+                          <div className="mt-2">
+                            <div className="flex flex-col space-y-1.5 mb-3">
+                              <Label htmlFor="name">Tournament Name</Label>
+                              <Input id="name" placeholder="" />
+                            </div>
+                            <div className="flex flex-col space-y-1.5 mb-3">
+                              <Label htmlFor="remark">Remarks</Label>
+                              <Input id="remark" placeholder="" />
+                            </div>
+                            <div className="flex flex-col space-y-1.5 mb-3 w-fit">
+                              <Label htmlFor="startTime">Tournament Start DateTime</Label>
+                              <Input id="startTime" type="datetime-local" />
+                            </div>
+                            <div className="flex flex-col space-y-1.5 mb-3 w-fit">
+                              <Label htmlFor="endTime">Tournament End DateTime</Label>
+                              <Input id="endTime" type="datetime-local" />
+                            </div>
                           </div>
                         </div>
-                      </Dialog>
+                      </div>
+                    </div>
+                    <div className="bg-black/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                      <button
+                        type="button"
+                        onClick={() => setCreate(false)}
+                        className="inline-flex w-full justify-center rounded-md bg-purple-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-600 sm:ml-3 sm:w-auto"
+                      >
+                        Create
+                      </button>
+                      <button
+                        type="button"
+                        data-autofocus
+                        onClick={() => setCreate(false)}
+                        className="mt-3 border border-purple-500 text-purple-500 transition-all duration-200 hover:bg-gray-300/70 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm sm:mt-0 sm:w-auto"
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </DialogPanel>
+                </div>
+              </div>
+            </Dialog>
           </CardHeader>
           <CardContent>
             {tournaments.map((tournament) => (
@@ -142,12 +158,9 @@ export default function AdminPage() {
                 </CardHeader>
                 <CardContent className="flex items-center justify-between">
                   <div>
-                    <span className="hidden sm:inline">
-                      Estimated Start-End Date:{' '}
-                    </span>
                     <span>
-                      {tournament.tournament_start} -{' '}
-                      {tournament.tournament_end}
+                      {convertDateTime(tournament.tournament_start)} -{' '}
+                      {convertDateTime(tournament.tournament_end)}
                     </span>
                     <div className="mt-2 text-xs">
                       Status:{' '}
@@ -161,23 +174,82 @@ export default function AdminPage() {
                     </div>
                   </div>
                   <div className="flex gap-2">
-                    <Link href="/adminEdit">
-                      <Button
-                        variant="outline"
-                        className="border-purple-500 text-purple-500 transition-all duration-200 hover:bg-gray-300/70"
-                      >
-                        Edit
-                      </Button>
-                    </Link>
+                    <Button
+                      variant="outline"
+                      className="border-purple-500 text-purple-500 transition-all duration-200 hover:bg-gray-300/70"
+                      onClick={() => setOpenEdit(tournament.tournament_id)}
+                    >
+                      Edit
+                    </Button>
+                    <Dialog open={openEdit === tournament.tournament_id} onClose={handleCloseEdit} className="relative z-10 rounded-lg">
+                      <DialogBackdrop
+                        transition
+                        className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+                      />
+
+                      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                          <DialogPanel
+                            transition
+                            className="relative transform overflow-hidden rounded-lg bg-[#1c1132] text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+                          >
+                            <div className="bg-[#1c1132] px-4 pb-4 pt-5 sm:p-6 sm:pb-4 text-white">
+                              <div className="sm:items-start">
+                                <div className="mt-3 text-center sm:mt-0 sm:text-left">
+                                  <DialogTitle as="h3" className="text-base font-semibold leading-6 text-white">
+                                    Edit Tournament
+                                  </DialogTitle>
+                                  <div className="mt-2">
+                                    <div className="flex flex-col space-y-1.5 mb-3">
+                                      <Label htmlFor="name">Tournament Name</Label>
+                                      <Input id="name" defaultValue={tournament.tournament_name}/>
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5 mb-3">
+                                      <Label htmlFor="remark">Remarks</Label>
+                                      <Input id="remark" defaultValue={tournament.remarks}/>
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5 mb-3 w-fit">
+                                      <Label htmlFor="startTime">Tournament Start DateTime</Label>
+                                      <Input id="startTime" type="datetime-local" defaultValue={tournament.tournament_start}/>
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5 mb-3 w-fit">
+                                      <Label htmlFor="endTime">Tournament End DateTime</Label>
+                                      <Input id="endTime" type="datetime-local" defaultValue={tournament.tournament_end}/>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            <div className="bg-black/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                              <button
+                                type="button"
+                                onClick={() => setOpenEdit(undefined)}
+                                className="inline-flex w-full justify-center rounded-md bg-purple-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-600 sm:ml-3 sm:w-auto"
+                              >
+                                Create
+                              </button>
+                              <button
+                                type="button"
+                                data-autofocus
+                                onClick={() => setOpenEdit(undefined)}
+                                className="mt-3 border border-purple-500 text-purple-500 transition-all duration-200 hover:bg-gray-300/70 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm sm:mt-0 sm:w-auto"
+                              >
+                                Cancel
+                              </button>
+                            </div>
+                          </DialogPanel>
+                        </div>
+                      </div>
+                    </Dialog>
                     <>
                       <Button
                         variant="outline"
                         className="border-purple-500 text-purple-500 transition-all duration-200 hover:bg-gray-300/70"
-                        onClick={() => setOpen(tournament.tournament_id)}
+                        onClick={() => setOpenDelete(tournament.tournament_id)}
                       >
                         Delete
                       </Button>
-                      <Dialog open={open === tournament.tournament_id} onClose={handleClose} className="relative z-10 rounded-lg">
+                      <Dialog open={openDelete === tournament.tournament_id} onClose={handleCloseDelete} className="relative z-10 rounded-lg">
                         <DialogBackdrop
                           transition
                           className="fixed inset-0 bg-gray-500 bg-opacity-10 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
@@ -210,7 +282,7 @@ export default function AdminPage() {
                               <div className="bg-black/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                                 <button
                                   type="button"
-                                  onClick={() => setOpen(false)}
+                                  onClick={() => setOpenDelete(false)}
                                   className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
                                 >
                                   Delete
@@ -218,7 +290,7 @@ export default function AdminPage() {
                                 <button
                                   type="button"
                                   data-autofocus
-                                  onClick={() => setOpen(false)}
+                                  onClick={() => setOpenDelete(false)}
                                   className="mt-3 border border-purple-500 text-purple-500 transition-all duration-200 hover:bg-gray-300/70 inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-50 sm:mt-0 sm:w-auto"
                                 >
                                   Cancel
